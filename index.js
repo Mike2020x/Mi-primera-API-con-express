6419bbbaf370f8d67193c807382792a208c4b4b2
Mike2020x <62442141+Mike2020x@users.noreply.github.com>
2023-05-04 20:00:57 -0500

    Refactorizacion del codigo usando modulos

--- index.js
+++ index.js
@@ -1,93 +1,35 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 
 const app = express();
 const port = 3001;
+
+const {
+  handleGetAllData,
+  handleGetInfo,
+  handleGetDataById,
+  handleDeleteData,
+  handleCreateData,
+  handleUpdateData,
+} = require("./controller");
 //midlewares
+app.use(cors());
 app.use(express.json());
 app.use(morgan("tiny"));
 app.use(express.static("build"));
 
-let persons = [
-  {
-    id: "1",
-    name: "Ana",
-    number: "555566644",
-  },
-  {
-    id: "2",
-    name: "Raul",
-    number: "555566644",
-  },
-  {
-    id: "3",
-    name: "Camilo",
-    number: "555566644",
-  },
-  {
-    id: "4",
-    name: "Juan",
-    number: "555566644",
-  },
-  {
-    id: "5",
-    name: "Michael",
-    number: "555566644",
-  },
-];
-
 //get info
-app.get("/info", (req, res) =>
-  res.send(
-    `Phonebook has info for ${persons.length} people <br> <br> ${new Date()}`
-  )
-);
+app.get("/info", handleGetInfo);
 //get persons
-app.get("/api/persons", (req, res) => {
-  res.json(persons);
-});
+app.get("/api/persons", handleGetAllData);
 //get person
-app.get("/api/persons/:id", (req, res) => {
-  const { id } = req.params;
-  const person = persons.find((person) => person.id === id);
-  if (!person) {
-    res.status(404).json({ message: "Person not found" });
-  }
-  res.json(person);
-});
+app.get("/api/persons/:id", handleGetDataById);
 //delete person
-app.delete("/api/persons/:id", (req, res) => {
-  const { id } = req.params;
-  const person = persons.filter((person) => person.id !== id);
-  res.status(204).end();
-});
-
+app.delete("/api/persons/:id", handleDeleteData);
 //add person
-app.post("/api/persons", (req, res) => {
-  const body = req.body;
+app.post("/api/persons", handleCreateData);
 
-  if (!body.name || !body.number) {
-    return res.status(400).json({
-      error: "name or number is missing",
-    });
-  }
+app.patch("/api/persons/:id", handleUpdateData);
 
-  const existingPerson = persons.find((person) => person.name === body.name);
-
-  if (existingPerson) {
-    return res.status(400).json({
-      error: "name must be unique",
-    });
-  }
-
-  const newPerson = {
-    id: Math.floor(Math.random() * 100000),
-    name: body.name,
-    number: body.number,
-  };
-
-  persons = persons.concat(newPerson);
-
-  res.json(newPerson);
-});
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => console.log(`App listening on port ${port}!`));
